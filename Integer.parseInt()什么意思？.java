990414f47a7c08dbf377b165c99c28447e881517
Neil <87235455+HaloNeil@users.noreply.github.com>
2021-08-05 16:40:38 +0800

    Integer.parseInt(

---
+++ Integer.parseInt()什么意思？.java
@@ -0,0 +1,8 @@
+java中的语句integer.parseint（)是将整型数据Integer转换为基本数据类型int
+例子：
+String str = ＂3＂;
+int i = Integer.parseInt(str);
+System.out.println(i+1)；//输出4;
+String str = ＂3＂;
+System.out.println(str+1);//output 31;
+integer，整数 / 整型数，与小数 / 浮点数相对，是编程语言的基本数据类型之一，用以指示变量的数据类型，有时也用于常量（变量、表达式）的强制数据类型转换。整型数据的长度及其取值范围并不固定，受编译环境影响。
